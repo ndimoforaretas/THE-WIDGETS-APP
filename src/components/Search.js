3f3ca182e610f4088294cecdac70d301e3390d66
ndimoforaretas <ndimaret@gmail.com>
2022-06-29 20:18:35 +0200

    linked to wikipedia page

--- src/components/Search.js
+++ src/components/Search.js
@@ -25,6 +25,17 @@ const Search = () => {
 		search();
 	}, [term]);
 
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className="item">
+				<div className="content">
+					<div className="header">{result.title}</div>
+					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+				</div>
+			</div>
+		);
+	});
+
 	return (
 		<div>
 			<div className="ui form">
@@ -37,6 +48,7 @@ const Search = () => {
 					/>
 				</div>
 			</div>
+			<div className="ui celled list">{renderedResults}</div>
 		</div>
 	);
 };
