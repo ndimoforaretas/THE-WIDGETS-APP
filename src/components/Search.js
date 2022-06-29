a641f3c6fc5a0612510d25697f44ce8aa097d2b2
ndimoforaretas <ndimaret@gmail.com>
2022-06-29 19:22:24 +0200

    axios installed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-	return <h1>Search</h1>;
+	const [term, setTerm] = useState("");
+
+	console.log("I RUN WITH EVERY RERENDER");
+
+	useEffect(() => {
+		console.log("I ONLY RUN ONCE");
+	}, [term]);
+
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Search Term</label>
+					<input
+						className="input"
+						value={term}
+						onChange={(e) => setTerm(e.target.value)}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Search;
