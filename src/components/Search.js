afabe7e45dcf855c77507ec90b1c775f33029e5a
ndimoforaretas <ndimaret@gmail.com>
2022-07-02 22:43:10 +0200

    use effect configuration complete

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,8 +5,6 @@ const Search = () => {
 	const [term, setTerm] = useState("programming");
 	const [results, setResults] = useState([]);
 
-	console.log(results);
-
 	useEffect(() => {
 		const search = async () => {
 			const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -22,12 +20,32 @@ const Search = () => {
 			setResults(data.query.search);
 		};
 
-		search();
+		if (term && !results.length) {
+			search();
+		} else {
+			const timeoutId = setTimeout(() => {
+				if (term) {
+					search();
+				}
+			}, 1000);
+
+			return () => {
+				clearTimeout(timeoutId);
+			};
+		}
 	}, [term]);
 
 	const renderedResults = results.map((result) => {
 		return (
 			<div key={result.pageid} className="item">
+				<div className="right floated content">
+					<a
+						className="ui button"
+						href={`https://en.wikipedia.org?curid=${result.pageid}`}
+					>
+						Go
+					</a>
+				</div>
 				<div className="content">
 					<div className="header">{result.title}</div>
 					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
