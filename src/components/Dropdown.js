c535ad05462d8e40412d964438f7f28e929fb92a
ndimoforaretas <ndimaret@gmail.com>
2022-07-03 15:31:08 +0200

    dropdown open and close fixed

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+	const [open, setOpen] = useState(false);
+	const ref = useRef();
+
+	useEffect(() => {
+		document.body.addEventListener(
+			"click",
+			(event) => {
+				if (ref.current.contains(event.target)) {
+					return;
+				}
+				setOpen(false);
+			},
+			{ capture: true }
+		);
+	}, []);
+
 	const renderedOptions = options.map((option) => {
 		if (option.value === selected.value) {
 			return null;
@@ -17,13 +33,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
 	});
 
 	return (
-		<div className="ui form">
+		<div ref={ref} className="ui form">
 			<div className="field">
 				<label className="label">Select a Color</label>
-				<div className="ui selection dropdown visible active">
+				<div
+					onClick={() => setOpen(!open)}
+					className={`ui selection dropdown ${open ? "visible active" : ""}`}
+				>
 					<i className="dropdown icon"></i>
 					<div className="text">{selected.label}</div>
-					<div className="menu visible transition">{renderedOptions}</div>
+					<div className={`menu ${open ? "visible transition" : ""}`}>
+						{renderedOptions}
+					</div>
 				</div>
 			</div>
 		</div>
